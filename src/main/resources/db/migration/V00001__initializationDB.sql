45a14eda3b8ac5d38bcc65fd33546d5281a22e56
PummelerDev <PummelerDev@users.noreply.github.com>
2023-04-05 21:23:54 +0300

    update tables in DB and migration.

--- src/main/resources/db/migration/V00001__initializationDB.sql
+++ src/main/resources/db/migration/V00001__initializationDB.sql
@@ -2,28 +2,15 @@ create sequence orders_id_seq1;
 
 alter sequence orders_id_seq1 owner to postgres;
 
-create table if not exists storages
-(
-    id      bigserial
-        constraint storages_pk
-            primary key,
-    name    varchar,
-    created timestamp default now(),
-    changed timestamp default now()
-);
-
-alter table storages
-    owner to postgres;
-
 create table if not exists manufacturers
 (
     id      bigserial
-        constraint manufacturers_pk
-            primary key,
+    constraint manufacturers_pk
+    primary key,
     name    varchar,
     created timestamp default now(),
     changed timestamp default now()
-);
+    );
 
 alter table manufacturers
     owner to postgres;
@@ -31,8 +18,8 @@ alter table manufacturers
 create table if not exists customers
 (
     id              bigserial
-        constraint customers_pk
-            primary key,
+    constraint customers_pk
+    primary key,
     first_name      varchar,
     last_name       varchar,
     address         varchar,
@@ -44,7 +31,7 @@ create table if not exists customers
     created         timestamp        default now(),
     changed         timestamp        default now(),
     is_deleted      boolean          default false
-);
+    );
 
 alter table customers
     owner to postgres;
@@ -52,61 +39,45 @@ alter table customers
 create table if not exists products
 (
     id              bigserial
-        constraint products_pk
-            primary key,
+    constraint products_pk
+    primary key,
     kind            varchar,
     topping         varchar,
     manufacturer_id bigint
-        constraint products_manufacturers_id_fk
-            references manufacturers,
+    constraint products_manufacturers_id_fk
+    references manufacturers
+    on update cascade on delete cascade,
     name            varchar,
     description     varchar,
     weight          integer,
     price           double precision,
     created         timestamp default now(),
     changed         timestamp default now()
-);
+    );
 
 alter table products
-    owner to postgres;
-
-create table if not exists l_storages_products_value
-(
-    storage_id bigint
-        constraint l_storages_products_value_storages_id_fk
-            references storages,
-    product_id bigint
-        constraint l_storages_products_value_products_id_fk
-            references products,
-    quantity   integer,
-    id         bigserial
-        constraint l_storages_products_value_pk
-            primary key,
-    created    timestamp default now(),
-    changed    timestamp default now()
-);
-
-alter table l_storages_products_value
     owner to postgres;
 
 create table if not exists orders
 (
     id           bigint    default nextval('orders_id_seq1'::regclass) not null
-        constraint orders_pk
-            primary key,
+    constraint orders_pk
+    primary key,
     order_number bigint,
     product_id   bigint
-        constraint orders_products_id_fk
-            references products,
+    constraint orders_products_id_fk
+    references products
+    on update cascade on delete cascade,
     customer_id  bigint
-        constraint orders_customers_id_fk
-            references customers,
+    constraint orders_customers_id_fk
+    references customers
+    on update cascade on delete cascade,
     quantity     integer,
     created      timestamp default now(),
     changed      timestamp default now(),
     cancelled    boolean   default false,
     finished     boolean   default false
-);
+    );
 
 alter table orders
     owner to postgres;
